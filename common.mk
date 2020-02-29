cd02b013d2dbb03585c2ab1cd8b2ec891d026d8e
Daniel Rosenberg <drosen@google.com>
2020-02-28 17:55:47 -0800

    sm8150-common: Enable casefolding support on userdata
    This enables casefolding, which is required for removal of sdcardfs.
    
    Test: Wipe data and boot. lsattr /data/media, check for +F
    Bug: 138322712
    Change-Id: I020aa6bb9c121e7bda83c07bc9fb37dcfa26702c
    Signed-off-by: neroices <git@slce.moe>

--- common.mk
+++ common.mk
@@ -21,6 +21,9 @@ PRODUCT_COMPRESSED_APEX := false
 # Include common vendor stuff
 $(call inherit-product, vendor/sony/sm8150-common/sm8150-common-vendor.mk)
 
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # VNDK
 BOARD_SHIPPING_API_LEVEL := 30
 PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
