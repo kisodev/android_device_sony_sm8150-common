be0287a6a4c1dae63bbb614911b8dc5187681376
Danny Trunk <dtrunk90@gmail.com>
2022-01-28 15:37:29 +0100

    sm8150-common: Add shim for missing symbol in lib-imsvt.so
    Fixes:
    
        E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: cannot locate symbol "_ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb" referenced by "/system_ext/lib64/lib-imsvideocodec.so"...
    
    Change-Id: I4364a1e1da2ae2120194f44c233f1f8b1cd0f7ac

--- extract-files.sh
+++ extract-files.sh
@@ -66,6 +66,9 @@ function blob_fixup() {
     product/lib64/libdpmframework.so)
         sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
         ;;
+    system_ext/lib64/lib-imsvideocodec.so )
+        "${PATCHELF}" --add-needed "libandroid.so" --add-needed "lib-imsvtshim.so" "${2}"
+        ;;
     esac
 }
 
