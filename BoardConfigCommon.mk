a62452a905ca3278ed15682fb45fa6fcdeeed53a
Alexander Koskovich <akoskovich@pm.me>
2022-04-26 21:18:08 -0700

    sm8150-common: Remove virtual framebuffer
     * It's largely for testing purposes and it consumes
       a large amount of memory. If your hardware doesn't
       have graphics then it can be useful, which isn't the
       case for shipped devices.
    
    Change-Id: I2348718ad3afba846498572161fa7e83dd8b7ce9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -49,7 +49,7 @@ TARGET_NO_BOOTLOADER := true
 
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 2
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=16 androidboot.usbcontroller=a600000.dwc3 buildid=KUMANO-1.2.0-211005-1847 panic_on_err=1
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=16 androidboot.usbcontroller=a600000.dwc3 buildid=KUMANO-1.2.0-211005-1847 panic_on_err=1
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_RAMDISK_OFFSET := 0x01000000
