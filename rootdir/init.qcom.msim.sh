8999a0dc197c5830ad2766cd740e5dd56ab3c0bb
voidanix <voidanix@keyedlimepie.org>
2024-07-08 21:15:28 +0200

    sm8150-common: Force-grep a model from LTALabel
    This should cover all JP models for a working NFC config. Since with
    dynamic partitions we cannot remount vendor and adjust the NFC config,
    desperately try to extract a matching model number from LTALabel.
    
    Signed-off-by: voidanix <voidanix@keyedlimepie.org>

--- rootdir/init.qcom.msim.sh
+++ rootdir/init.qcom.msim.sh
@@ -1,6 +1,11 @@
 #!/vendor/bin/sh
 
-model=`grep -aim1 'model:' /dev/block/bootdevice/by-name/LTALabel | sed -e 's/^.*model:[ ]*\([A-Za-z0-9-]*\).*$/\1/I'` 2> /dev/null
+model=$(grep -aim1 'model:' /dev/block/bootdevice/by-name/LTALabel | sed -e 's/^.*model:[ ]*\([A-Za-z0-9-]*\).*$/\1/I') 2> /dev/null
+
+# Desperate measures
+if [ "$model" == "" ]; then
+    model=$(strings /dev/block/bootdevice/by-name/LTALabel | grep -m 1 -oE '(SO-01M|SOV41|901SO|SO-03L|SOV40|802SO)') 2> /dev/null
+fi
 
 case "$model" in
     "J9110" | "J9150" | "J9180" | "J9210" | "J9260")
