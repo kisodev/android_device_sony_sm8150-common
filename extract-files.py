682a355d1b2ae6bed299d01ca07ae10a2f9bd087
voidanix <voidanix@keyedlimepie.org>
2024-12-27 01:42:47 +0100

    sm8150-common: Patch more system WFD stack blobs
    Addresses some clashing references and missing symbols.
    
    Signed-off-by: voidanix <voidanix@keyedlimepie.org>

--- extract-files.py
+++ extract-files.py
@@ -94,6 +94,13 @@ blob_fixups: blob_fixups_user_type = {
             'vendor/lib/libwvhidl.so',
         ): blob_fixup()
             .add_needed('libcrypto_shim.so'),
+        'system_ext/lib/libwfdservice.so': blob_fixup()
+            .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
+        'system_ext/lib64/libwfdnative.so': blob_fixup()
+            .add_needed('libbinder_shim.so')
+            .add_needed('libinput_shim.so'),
+        'system_ext/lib/libwfdmmsrc_system.so': blob_fixup()
+            .add_needed('libgui_shim.so'),
 }  # fmt: skip
 
 lib_fixups: lib_fixups_user_type = {
