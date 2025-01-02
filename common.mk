c686714e3bb96463c596817df261a337dfece1af
dianlujitao <dianlujitao@lineageos.org>
2025-01-02 11:34:02 +0800

    sm8150-common: move to aidl bootctrl hal
    Change-Id: I50fd09148097f0cda03c1a105340a3fd5f55ce8d

--- common.mk
+++ common.mk
@@ -213,9 +213,8 @@ PRODUCT_PACKAGES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery
 
 $(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
 
