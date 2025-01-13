08bd83b1e807b9d1b2a0ba056d4c9bf7ff69a179
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    sm8150-common: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- common.mk
+++ common.mk
@@ -341,6 +341,12 @@ PRODUCT_PACKAGES += \
     SecureElement \
     Tag
 
+# Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
